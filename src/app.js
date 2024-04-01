788ffeaeacc81a12e1a936dae8d555c47f1dae46
javidh0 <120474876+javidh0@users.noreply.github.com>
2024-04-01 21:57:11 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,12 +3,20 @@ const {authenticateUser, validateAuthentication} = require('./authentication/aut
 
 mongoose.connect("mongodb://127.0.0.1:27017/dsa_practice_project");
 
-authenticateUser(
-    {
-        'user_id' : 'javidh@gmail.com',
-        'password' : 'pass1' 
-    },
-    (val) => {
-        console.log(val);
-    }
-)
+// authenticateUser(
+//     {
+//         'user_id' : 'javidh@gmail.com',
+//         'password' : 'pass1' 
+//     },
+//     (val) => {
+//         console.log(val);
+//     }
+// )
+
+// validateAuthentication(
+//     "javidh@gmai.com",
+//     '660ae0018e84b8d0180780dd',
+//     (val) => {
+//         console.log(val);
+//     }
+// )
