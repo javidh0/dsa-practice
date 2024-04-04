0b3f644e42e845831dd24cc749154e7a55714744
javidh0 <120474876+javidh0@users.noreply.github.com>
2024-04-04 22:51:15 +0530

    Update database.js

--- src/database.js
+++ src/database.js
@@ -63,15 +63,60 @@ const access_token = mongoose.model(
     }
 );
 
+// sheets.create({
+//     'name' : 'Faizaan Test Sheet',
+//     'question' : [
+//         {'name' : 'Faizaan test question 1'},
+//         {'name' : 'Faizaan test question 2'},
+//         {'name' : 'Faizaan test question 3'},
+//         {'name' : 'Faizaan test question 4'},
+//         {'name' : 'Faizaan test question 5'}
+//     ] 
+// })
+
 // users.create({
-//     'user_id' : 'faizaan@gmail.com',
+//     'user_id' : 'javidh10@gmail.com',
 //     'auth' : {
-//         'email' : 'faizaan@gmail.com',
+//         'email' : 'javidh10@gmail.com',
 //         'password' : 'pass1'
 //     },
-//     'name' : 'Faizaan',
-//     'age' : 22,
+//     'name' : 'Javidh',
+//     'age' : 21,
+//     'enrolled' : {
+//         "questions_data" : [],
+//         "sheets_data" : [
+//             {
+//                 'sheets_id' : "660eb50ec651d3798a5df75b",
+//                 'progress' : 1.5
+//             },
+//             {
+//                 "sheets_id" : "660eb54c4dfd2cb760370d4c",
+//                 'progress' : 10   
+//             }
+//         ]
+//     }
 // })
+
+// [
+//     {
+//       _id: ObjectId('660ae71b4e86d5ecbc3be70f'),
+//       user_id: 'javidh10@gmail.com',
+//       auth: { email: 'javidh10@gmail.com', password: 'pass1' },
+//       name: 'Javidh',
+//       age: '21',
+//       enrolled: { questions_data: [], sheets_data: [] },
+//       __v: 0
+//     },
+//     {
+//       _id: ObjectId('660b9989542f6d88e7492e00'),
+//       user_id: 'faizaan@gmail.com',
+//       auth: { email: 'faizaan@gmail.com', password: 'pass1' },
+//       name: 'Shaik Faizaan',
+//       age: '21',
+//       enrolled: { questions_data: [], sheets_data: [] },
+//       __v: 0
+//     }
+// ]
 
 module.exports = {
     users, access_token, sheets
