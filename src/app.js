6f5f78a1956d341e0996ea6adc20b2290ed648ad
javidh0 <120474876+javidh0@users.noreply.github.com>
2024-04-04 22:51:08 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const {authenticateUser, validateAuthentication} = require('./authentication/authenticate');
-const {signIn, getUser, signUp, updateUser} = require('./api/api.js');
+const {signIn, getUser, signUp, updateUser, getAllSheets, getSheet, sheetEnrolled} = require('./api/api.js');
 const express = require('express');
 const bodyParser = require('body-parser');
+
 
 mongoose.connect("mongodb://127.0.0.1:27017/dsa_practice_project");
 
@@ -40,7 +41,11 @@ app.listen(1729, () => {console.log('1729')});
 app.use(express.json())
 
 app.get('/users/:user_id', getUser); 
-app.get('/sign_in', signIn);
+app.post('/sign_in', signIn);
 app.post('/sign_up', signUp);
 app.patch('/users/update', updateUser);
 
+app.get('/get_all_sheets', getAllSheets);
+app.get('/get_sheet', getSheet);
+
+app.get('/get_enrolled', sheetEnrolled);
