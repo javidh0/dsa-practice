7366374562d0b1d4f7f45babcfc36b9e0de14f7f
javidh0 <120474876+javidh0@users.noreply.github.com>
2024-04-15 22:09:12 +0530

    Update g_widgets.dart

--- frontend/lib/utils/g_widgets.dart
+++ frontend/lib/utils/g_widgets.dart
@@ -3,6 +3,117 @@ import './g_functions.dart';
 
 void emptyCallback(val) {}
 
+void dummyCallback() {}
+
+class MyListForm extends StatelessWidget {
+  const MyListForm({
+    super.key,
+    required this.leading,
+    required this.hints,
+    required this.primaryClr,
+    required this.updateFuntion,
+    this.height = 50.0,
+    this.sumbitFuntion = dummyCallback,
+  });
+
+  final List<String> hints;
+  final Color primaryClr;
+  final Widget leading;
+  final Function updateFuntion, sumbitFuntion;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> widgets = [];
+    List<String> responses = [];
+    int n = hints.length;
+    widgets.add(leading);
+    for (int i = 0; i < n; i++) {
+      widgets.add(
+        MyBaseInputField(
+          text: hints[i],
+          onChange: (val) {
+            responses.add(val);
+            updateFuntion(i, val);
+          },
+        ),
+      );
+    }
+
+    widgets.add(
+      MyBaseButton(
+        onPressed: () {
+          // print(context.read<LoginData>().data);
+          sumbitFuntion();
+        },
+      ),
+    );
+    return Container(
+      height: getHeight(context, height),
+      decoration: BoxDecoration(
+        color: primaryClr,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: widgets,
+        ),
+      ),
+    );
+  }
+}
+
+class MyBaseButton extends StatefulWidget {
+  const MyBaseButton({
+    super.key,
+    this.onPressed = dummyCallback,
+  });
+
+  final Function onPressed;
+
+  @override
+  State<MyBaseButton> createState() => _MyBaseButtonState();
+}
+
+class _MyBaseButtonState extends State<MyBaseButton> {
+  bool isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton.icon(
+        onPressed: isLoading
+            ? null
+            : () {
+                setState(() {
+                  isLoading = true;
+                });
+                widget.onPressed();
+              },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.amber,
+          padding: const EdgeInsets.all(16.0),
+        ),
+        icon: isLoading
+            ? Container(
+                width: 24,
+                height: 24,
+                padding: const EdgeInsets.all(2.0),
+                child: const CircularProgressIndicator(
+                  color: Colors.white,
+                  strokeWidth: 3,
+                ),
+              )
+            : const Icon(Icons.feedback),
+        label: const Text('SUBMIT'),
+      ),
+    );
+  }
+}
+
 class MyBaseCard extends StatelessWidget {
   const MyBaseCard({
     super.key,
@@ -40,20 +151,23 @@ class MyBaseInputField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: getWidth(context, 20),
-      child: TextField(
-        onChanged: onChange,
-        decoration: InputDecoration(
-          border: const OutlineInputBorder(),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(25.0),
-            borderSide: BorderSide(
-              color: primaryClr,
-              width: 2.0,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 7.5, horizontal: 20),
+      child: SizedBox(
+        width: getWidth(context, 20),
+        child: TextField(
+          onChanged: onChange,
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25.0),
+              borderSide: BorderSide(
+                color: primaryClr,
+                width: 2.0,
+              ),
             ),
+            hintText: text,
           ),
-          hintText: text,
         ),
       ),
     );
