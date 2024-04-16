8ab06d25e89bb2f1ee93e77b6979a51b5e5d6d10
javidh0 <120474876+javidh0@users.noreply.github.com>
2024-04-16 23:12:41 +0530

    Update my_provider.dart

--- frontend/lib/login_signin/my_provider.dart
+++ frontend/lib/login_signin/my_provider.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
 class LoginData extends ChangeNotifier {
-  List<String> data = ["", "", ""];
-
+  List<String> data = ["", ""];
+  bool success = false;
   void init(int n) {
     for (int i = 0; i < n; i++) {
       data.add("");
     }
+  }
+
+  void setSuccess() {
+    success = true;
+    notifyListeners();
+  }
+
+  List<String> getData() {
+    return data;
   }
 
   void updateData(int i, String val) {
