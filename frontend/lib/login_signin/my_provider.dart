73c6d6eed146fcb87e01725ac6cc69937df1e185
javidh0 <120474876+javidh0@users.noreply.github.com>
2024-04-18 00:58:54 +0530

    Update my_provider.dart

--- frontend/lib/login_signin/my_provider.dart
+++ frontend/lib/login_signin/my_provider.dart
@@ -1,16 +1,38 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 class LoginData extends ChangeNotifier {
   List<String> data = ["", ""];
+  Map accessKey = {
+    "auth": {"user_id": "-1", "access_token": "-1"}
+  };
   bool success = false;
-  void init(int n) {
-    for (int i = 0; i < n; i++) {
-      data.add("");
-    }
+  bool wrong = false;
+  bool load = false;
+
+  void setLoad(bool val) {
+    load = val;
+    notifyListeners();
   }
 
-  void setSuccess() {
-    success = true;
+  void setSuccess(token) {
+    token = jsonDecode(token);
+
+    try {
+      if (token['auth']['access_token'] != "-1") {
+        accessKey = token;
+        success = true;
+        wrong = false;
+      } else {
+        token;
+        wrong = true;
+      }
+    } catch (e) {
+      token;
+      wrong = true;
+    }
+    load = false;
     notifyListeners();
   }
 
@@ -21,5 +43,59 @@ class LoginData extends ChangeNotifier {
   void updateData(int i, String val) {
     data[i] = val;
     notifyListeners();
+  }
+}
+
+class RegisterData extends ChangeNotifier {
+  List<String> data = ["", "", "", ""];
+
+  void updateData(int i, String val) {
+    data[i] = val;
+    notifyListeners();
+  }
+
+  bool success = false;
+  bool wrong = false;
+  bool load = false;
+
+  void setLoad(bool val) {
+    load = val;
+    notifyListeners();
+  }
+
+  void setSuccess(val) {
+    try {
+      var response = jsonDecode(val);
+
+      if (response['error'] != null) {
+        wrong = true;
+      } else {
+        wrong = false;
+        success = true;
+      }
+    } catch (e) {
+      wrong = false;
+      success = true;
+    }
+    load = false;
+    notifyListeners();
+  }
+
+  bool checkRequired() {
+    for (int i = 0; i < 4; i++) {
+      if (data[i] == "") return false;
+    }
+    return true;
+  }
+
+  Map getData() {
+    Map tr = {
+      'user_id': data[0],
+      'auth': {'email': data[0], 'password': data[1]},
+      'name': data[2],
+      'age': data[3]
+    };
+
+    return tr;
   }
 }
