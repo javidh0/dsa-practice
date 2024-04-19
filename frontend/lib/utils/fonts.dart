e4a45c5e9442754bbefadd696e3d67cf0192a207
javidh0 <120474876+javidh0@users.noreply.github.com>
2024-04-19 22:53:39 +0530

    Create fonts.dart

---
+++ frontend/lib/utils/fonts.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle textFont = GoogleFonts.roboto();
+
+TextStyle textTitle = GoogleFonts.roboto(
+  fontWeight: FontWeight.w500,
+  fontSize: 30,
+);
